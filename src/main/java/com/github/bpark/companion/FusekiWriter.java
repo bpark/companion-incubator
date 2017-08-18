c1b43b28ebed4b4918b910b2086c9fbdb199947a
Kurt Sparber <ksr@DGKHQ0ERDHJW.lan.muc.financial.com>
2017-08-18 15:25:35 +0200

    added fuseki writer client

---
+++ src/main/java/com/github/bpark/companion/FusekiWriter.java
@@ -0,0 +1,36 @@
+package com.github.bpark.companion;
+
+import org.eclipse.rdf4j.query.QueryLanguage;
+import org.eclipse.rdf4j.query.Update;
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
+
+/**
+ * @author ksr
+ */
+public class FusekiWriter {
+
+    public static void main(String[] args) {
+        String sparqlEndpoint = "http://192.168.56.8:3030/demo/update";
+        Repository repo = new SPARQLRepository(sparqlEndpoint);
+        repo.initialize();
+
+
+        String query = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+                "INSERT\n" +
+                "        {\n" +
+                "            <http://localhost/me> foaf:name \"Donald Trump\"\n" +
+                "        }\n" +
+                "        WHERE {}";
+
+        System.out.println(query);
+
+        try (RepositoryConnection conn = repo.getConnection()) {
+
+            Update update = conn.prepareUpdate(QueryLanguage.SPARQL, query);
+            update.execute();
+        }
+
+    }
+}
