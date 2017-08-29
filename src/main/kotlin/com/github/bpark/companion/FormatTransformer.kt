ef199059ae46e19f7af6ef571d34ec0286ec4fb3
bpark <burt.parkers@googlemail.com>
2017-08-29 16:35:41 +0200

    added more datasets

---
+++ src/main/kotlin/com/github/bpark/companion/FormatTransformer.kt
@@ -0,0 +1,18 @@
+package com.github.bpark.companion
+
+import org.eclipse.rdf4j.rio.RDFFormat
+import org.eclipse.rdf4j.rio.Rio
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+
+fun main(args: Array<String>) {
+    val rdfParser = Rio.createParser(RDFFormat.TURTLE)
+
+    val inStream = FileInputStream("data/foaf.rdf")
+
+    // Rio also accepts a java.io.Reader as input for the parser.
+    val model = Rio.parse(inStream, "", RDFFormat.RDFXML)
+
+    Rio.write(model, FileOutputStream("data/foaf.ttl"), RDFFormat.TURTLE);
+}
--- src/main/kotlin/com/github/bpark/companion/FusekiUpload.kt
+++ src/main/kotlin/com/github/bpark/companion/FusekiUpload.kt
@@ -6,13 +6,13 @@ import java.io.File
 import java.io.IOException
 
 fun main(args: Array<String>) {
-    val sparqlEndpoint = "http://192.168.56.8:8080/demo/update"
+    val sparqlEndpoint = "http://192.168.56.8:3030/demo/update"
     val repo = SPARQLRepository(sparqlEndpoint)
     repo.initialize()
 
 
     try {
-        repo.connection.use { conn -> conn.add(File("data/skos-demo.ttl"), null, RDFFormat.TURTLE) }
+        repo.connection.use { conn -> conn.add(File("data/foaf-demo.ttl"), null, RDFFormat.TURTLE) }
     } catch (e: IOException) {
         throw RuntimeException(e)
     }
