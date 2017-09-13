6a352635d0d3a71e454539272daa38509679e8d1
bpark <burt.parkers@googlemail.com>
2017-09-13 16:50:59 +0200

    added more tenses

--- src/main/kotlin/com/github/bpark/companion/TenseDetection.kt
+++ src/main/kotlin/com/github/bpark/companion/TenseDetection.kt
@@ -19,7 +19,9 @@ data class WordInfo(val token: String, val tag: String, val lemma: String?) {
     fun map(): String {
         return when {
             (tag == "VBP" || tag == "VBZ") && lemma == "be" -> "$tag($lemma)"
-            tag == "VBD" && lemma == "be" -> "$tag($lemma)"
+            (tag == "VBP" || tag == "VBZ") && lemma == "have" -> "$tag($lemma)"
+            (tag == "VBD" || tag == "VBN") && lemma == "be" -> "$tag($lemma)"
+            tag == "VBD" && lemma == "have" -> "$tag($lemma)"
             tag == "VBP" || tag == "VBG" || tag == "VBZ" -> tag
             else -> tag
         }
@@ -41,6 +43,26 @@ object TenseDetection {
 
     val pastProgressive = listOf<String>("I was working.", "I wasn't working.", "Was I working?", "He was working.", "He wasn't working.",
             "Was he working?", "I was going.", "I wasn't going.", "Was I going?", "He was going.", "He wasn't going.", "Was he going?")
+
+    val simplePresentPerfect = listOf<String>("I have worked.", "I haven't worked.", "Have I worked?", "He has worked.", "He hasn't worked.", "Has he worked?",
+            "I have gone.", "I haven't gone.", "Have I gone?", "He has gone.", "He hasn't gone.", "Has he gone?")
+
+    val presentPerfectProgressive = listOf<String>("I have been working.", "I haven't been working.", "Have I been working?", "He has been working.",
+            "He hasn't been working.", "Has he been working?", "I have been going.", "I haven't been going.", "Have I been going?", "He has been going.",
+            "He hasn't been going.", "Has he been going?")
+
+    val simplePastPerfect = listOf<String>("I had worked.", "I hadn't worked.", "Had I worked?", "He had worked.", "He hadn't worked.", "Had he worked?",
+            "I had gone.", "I hadn't gone.", "Had I gone?", "He had gone.", "He hadn't gone.", "Had he gone?")
+
+    val pastPerfectProgressive = listOf<String>("I had been working.", "I hadn't been working.", "Had I been working?", "He had been working.",
+            "He hadn't been working.", "Had he been working?", "I had been going.", "I hadn't been going.", "Had I been going?", "He had been going.",
+            "He hadn't been going.", "Had he been going?")
+
+    val willFuture = listOf<String>("I'll work.", "I won't work.", "Will I work?", "He'll work.", "He won't work.", "Will he work?", "I'll go.",
+            "I won't go.", "Will I go?", "He'll go.", "He won't go.", "Will he go?")
+
+    val tensePhrases = listOf<List<String>>(simplePresent, presentProgressive, simplePast, pastProgressive,
+            simplePresentPerfect, presentPerfectProgressive, simplePastPerfect, pastPerfectProgressive, willFuture)
 
     val path = "dict"
     val dictionary = Dictionary(File(path))
@@ -68,6 +90,7 @@ object TenseDetection {
             run {
                 val token = tokens[index]
                 val word = removeContractions(token)
+                if (token == "won't") println(word)
                 var lemma: String? = null
 
                 val pos = PosType.byPennTag(tag)
@@ -98,6 +121,7 @@ object TenseDetection {
 
         var normalForm = inputString
 
+        normalForm = normalForm.replace("won't".toRegex(), "will not")
         normalForm = normalForm.replace("n't".toRegex(), " not")
         normalForm = normalForm.replace("'re".toRegex(), " are")
         normalForm = normalForm.replace("'m".toRegex(), " am")
@@ -116,28 +140,13 @@ object TenseDetection {
 
 fun main(args: Array<String>) {
 
-    TenseDetection.simplePresent.forEach {
-        val wordinfos = TenseDetection.buildWordInfo(it)
-        val bags = TenseDetection.buildBag(wordinfos)
-        println("$it: $bags -> $wordinfos")
-    }
-    println()
-    TenseDetection.presentProgressive.forEach {
-        val wordinfos = TenseDetection.buildWordInfo(it)
-        val bags = TenseDetection.buildBag(wordinfos)
-        println("$it: $bags -> $wordinfos")
-    }
-    println()
-    TenseDetection.simplePast.forEach {
-        val wordinfos = TenseDetection.buildWordInfo(it)
-        val bags = TenseDetection.buildBag(wordinfos)
-        println("$it: $bags -> $wordinfos")
-    }
-    println()
-    TenseDetection.pastProgressive.forEach {
-        val wordinfos = TenseDetection.buildWordInfo(it)
-        val bags = TenseDetection.buildBag(wordinfos)
-        println("$it: $bags -> $wordinfos")
+    TenseDetection.tensePhrases.forEach {
+        it.forEach {
+            val wordinfos = TenseDetection.buildWordInfo(it)
+            val bags = TenseDetection.buildBag(wordinfos)
+            println("$it: $bags -> $wordinfos")
+        }
+        println()
     }
 
 }
